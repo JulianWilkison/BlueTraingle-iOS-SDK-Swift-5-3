5a2cc0509f9770b8e242d179e1576586f7d97577
Julian Wilkison <julian.wilkison@bluetriangle.com>
2020-11-02 08:22:30 -0500

    Initial Commit

---
+++ TestBlueTrainglePod/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestBlueTrainglePod
+//
+//  Created by Julian Wilkison-Duran on 11/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestBlueTrainglePod/TestBlueTrainglePodApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBlueTrainglePodApp.swift
+//  TestBlueTrainglePod
+//
+//  Created by Julian Wilkison-Duran on 11/2/20.
+//
+
+import SwiftUI
+
+@main
+struct TestBlueTrainglePodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
